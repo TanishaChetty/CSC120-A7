f6401abaca7778d8d93aa2582d6ac82da469ccd2
Tanisha Chetty <tchetty@smith.edu>
2025-03-31 23:06:58 -0400

    #SUBMIT!

--- House.java
+++ House.java
@@ -1,12 +1,68 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+
+/* This is a stub for the House class */
+
+
+public class House extends Building {
+  // Attributes
+  private ArrayList<Student> residents;
+  private Boolean hasDiningRoom;
+
+/**
+  * Constructor for House
+  * @param name name of House
+  * @param address address of House
+  * @param nFloors number of floors in House
+  * @param hasDiningRoom whether or not the House has a dining room
+  */  
+public House(String name, String address, int nFloors, Boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<Student>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
   }
 
+  /**
+  * Accessor to see if the House has a dining room
+  */
+  public Boolean getHasDiningRoom(){
+    return this.hasDiningRoom;
+    }
+
+  /**
+  * Accessor to see how many people live in the House
+  */
+  public Integer nResidents() {
+    return this.residents.size();
+  }
+
+  /**
+  * Updates the residents when someone moves in
+  */
+  public void moveIn(Student s){
+    this.residents.add(s);
+  }
+
+
+  /**
+  * Updates the residents when someone moves out, returns the student
+  */
+  public Student moveOut(Student s){
+    if (residents.contains(s)){
+      this.residents.remove(s);
+    }
+    return (s);
+  }
+
+  
+
+
+
+
   public static void main(String[] args) {
-    new House();
+    new House("Talbot House", "25 Prospect Street", 4, false);
   }
 
 }
+
--- reflection.md
+++ reflection.md
@@ -2,3 +2,5 @@ Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I thought the concept of Inheritance was interesting and made it easier to make the different buildings. It was cool to see how I could make multiple different classes that all related to each other. For this assignment especially, I did a lotttt of looking up the java documentation online, espeically for the newer stuff like hashtables, which was really helpful. Honestly, I was kind of nervous about this assignment because of the classes interacting and everything, but the assignment wasn't as crazy impossible as I thought it would be.
